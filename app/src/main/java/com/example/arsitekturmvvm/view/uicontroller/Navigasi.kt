183a74873a39bd44db7cee28286aa7569423bbbc
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-20 13:33:15 +0700

    tambah file Navigasi.kt

---
+++ app/src/main/java/com/example/arsitekturmvvm/view/uicontroller/Navigasi.kt
@@ -0,0 +1,53 @@
+package com.example.navigasi
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasiui.view.FormIsian
+import com.example.questnavigasiui.view.TampilData
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+            modifier = Modifier.padding(isiRuang)){
+            composable(route = Navigasi.Formulirku.name){
+                FormIsian(
+                    //pilihanJK = JenisK.map { id -> konteks.resources.getString}
+                    OnSubmitBtnClick={
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulirku(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulirku(
+    navController: NavHostController
+){
+    navController.popBackStack(Navigasi.Formulirku.name,
+    inclusive = false)
+}

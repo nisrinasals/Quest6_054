9833be18c77b630076e36087883af79db58c9b76
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-20 12:42:16 +0700

    membuat DataJK.kt

---
+++ app/src/main/java/com/example/arsitekturmvvm/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.arsitekturmvvm.model
+
+import com.example.arsitekturmvvm.R
+
+object DataJK {
+    val JenisK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}

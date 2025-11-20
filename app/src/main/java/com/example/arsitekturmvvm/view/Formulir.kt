153158a6d5f1910d48bc54c2306b4ab3d861524d
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-20 13:03:01 +0700

    menambah variabel  txtNama pada Formulir.kt

--- app/src/main/java/com/example/arsitekturmvvm/view/Formulir.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/Formulir.kt
@@ -21,12 +21,18 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+
 import com.example.arsitekturmvvm.R
 
 
@@ -37,6 +43,10 @@ fun FormIsian(
     OnSubmitBtnClick : (MutableList<String>) -> Unit,
     modifier: Modifier = Modifier
 ){
+
+    var txtNama by rememberSaveable { mutableStateOf("") }
+
+
     Scaffold (modifier= Modifier,
         {
             TopAppBar(
